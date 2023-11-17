b60e4440a0bbcd7b1615ac1501f75726965fcbee
window-ops <vladbruma2020@gmail.com>
2023-11-17 20:25:23 +0000

    Update 11 files
    - /2-18-ani/index.html
    - /2-18-ani/js/functii.js
    - /12-18-luni/index.html
    - /12-18-luni/js/functii.js
    - /css/stil.css
    - /despre/index.html
    - /export/index.html
    - /export/js/functii.js
    - /faq/index.html
    - /index.html
    - /Ordinul nr. 1068-627.pdf

---
+++ 12-18-luni/js/functii.js
@@ -0,0 +1,68 @@
+let punctaj = 0;
+let intrebaricomplete = 0;
+let nrintrebari = 11;
+let obtineparametriiurl = new URLSearchParams(window.location.search);
+let varsta = obtineparametriiurl.get('varsta');
+
+function extragevarstadinurlsiafiseaza() {
+  // Verifică dacă toate parametrii necesari există și sunt valizi
+  if (![varsta].every(Boolean) || isNaN(parseInt(varsta)) || varsta.trim() === "") {
+    alert("Chestionarul nu este valid, vă rugăm să reveniți de la început pentru a alege un chestionar corespunzător!");
+    window.location.href = "https://miculpionier.ro/govro/siui/chestionar-tsa";
+    return;
+  }
+
+  // Afișează
+  document.title = "Chestionar pentru depistarea TSA (" + varsta + ")";
+  document.getElementById("titlu").innerHTML = "Chestionar pentru depistarea TSA (" + varsta + ")";
+  
+  // Șterge parametrii din URL pentru a preveni modificarea
+  history.replaceState(null, null, window.location.pathname);
+}
+
+function raspunde(idintrebare, adaugarepunctaj) {
+  // Adaugă punctajul și marcheaza o întrebare ca fiind completă
+  punctaj = punctaj + adaugarepunctaj;
+  intrebaricomplete++;
+
+  // Dezactivează întrebarea
+  const intrebare = document.getElementById(idintrebare);
+  intrebare.classList.add("disabled-col");
+}
+
+function afiseazapnr() {
+  // Verifică daca s-a raspuns la toate întrebările
+  if (intrebaricomplete < nrintrebari) {
+    alert("Nu ați răspuns la toate întrebările!");
+  }
+  else if (intrebaricomplete == nrintrebari) {
+  	// Evaluează
+    if (0 <= punctaj && punctaj <= 6) {
+      document.getElementById("punctajafis").innerHTML = punctaj.toString();
+      document.getElementById("riscafis").innerHTML = "Risc minim";
+      document.getElementById("recomandariafis").innerHTML = "Reevaluare peste 3 luni";
+    }
+    else if (7 <= punctaj && punctaj <= 9) {
+      document.getElementById("punctajafis").innerHTML = punctaj.toString();
+      document.getElementById("riscafis").innerHTML = "Risc mediu";
+      document.getElementById("recomandariafis").innerHTML = "Reevaluare peste 3 luni";
+    }
+    else if (punctaj >= 10) {
+      document.getElementById("punctajafis").innerHTML = punctaj.toString();
+      document.getElementById("riscafis").innerHTML = "Risc sever";
+      document.getElementById("recomandariafis").innerHTML = "Trimitere către medicul de specialitate psihiatrie pediatrică / neuropsihiatrie pediatrică";
+    }
+
+  	//Ascunde afiseazapnr și afișează pnr
+  	document.getElementById("afiseazapnr").remove();
+  	document.getElementById("pnr").style.display = "block";
+  }
+}
+
+function refachestionarul() {
+  let confirmare = confirm("Sigur vreți să refaceți chestionarul? OK = DA; Cancel = NU");
+  if (confirmare) {
+    let url = "https://miculpionier.ro/govro/siui/chestionar-tsa/12-18-luni/?varsta=" + varsta;
+    window.location.href = url;
+  }
+}
---
+++ export/js/functii.js
@@ -0,0 +1,108 @@
+let punctajobtinut = "";
+let riscobtinut = "";
+let recomandariobtinute = "";
+let varstaobtinuta = "";
+let CUIfurnizor = "";
+let codparafamedic = "";
+let CNPcopil = "";
+let datacurenta = new Date();
+let luni = ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'];
+let ziua = datacurenta.getDate();
+let luna = luni[datacurenta.getMonth()];
+let anul = datacurenta.getFullYear();
+let dataformatata = ziua + ' ' + luna + ' ' + anul;
+let parametriiurl = new URLSearchParams(window.location.search);
+
+
+function export_construiesteurlsinavigheaza() {
+  let url = "https://miculpionier.ro/govro/siui/chestionar-tsa/export/?punctaj=" + punctaj + "&risc=" + document.getElementById("riscafis").innerHTML + "&recomandari=" + document.getElementById("recomandariafis").innerHTML + "&varsta=" + parametriiurl.get('varsta');
+  window.location.href = url;
+}
+
+function extragedatedinurlsiafiseaza() {
+  // Extrage date din URL
+  punctajobtinut = parametriiurl.get('punctaj');
+  riscobtinut = parametriiurl.get('risc');
+  recomandariobtinute = parametriiurl.get('recomandari');
+  varstaobtinuta = parametriiurl.get('varsta');
+  
+  // Verifică dacă toate parametrii necesari există și sunt valizi
+  if (![punctajobtinut, riscobtinut, recomandariobtinute, varstaobtinuta].every(Boolean) || isNaN(parseInt(punctajobtinut)) || isNaN(parseInt(varstaobtinuta)) || punctajobtinut.trim() === "" || riscobtinut.trim() === "" || recomandariobtinute.trim() === "" || varstaobtinuta.trim() === "") {
+    alert("Exportul nu este valid, vă rugăm să reveniți de la început pentru a alege un chestionar corespunzător!");
+    window.location.href = "https://miculpionier.ro/govro/siui/chestionar-tsa";
+    return;
+  }
+
+  // Afișează
+  document.getElementById("punctajafis").innerHTML = punctajobtinut;
+  document.getElementById("riscafis").innerHTML = riscobtinut;
+  document.getElementById("recomandariafis").innerHTML = recomandariobtinute;
+  document.getElementById("varstacopilinput").value = varstaobtinuta;
+  document.getElementById("datacompletariinput").value = dataformatata;
+  
+  // Șterge parametrii din URL pentru a preveni modificarea
+  history.replaceState(null, null, window.location.pathname);
+}
+
+
+function exportdate() {
+  // Previne comportamentul implicit de trimitere
+  event.preventDefault();
+  
+  // Atribuie valori introduse de utilizator
+  CUIfurnizor = document.getElementById("CUIinput").value;
+  codparafamedic = document.getElementById("codparafainput").value;
+  CNPcopil = document.getElementById("CNPcopilinput").value;
+
+  // Verifică dacă toate câmpurile sunt completate și completate corect
+  if (CUIfurnizor.length !== 12) {
+    alert("CUI-ul furnizorului trebuie să aibă 12 caractere!");
+    return;
+  }
+  else if (codparafamedic.length !== 6) {
+    alert("Codul parafă medic trebuie să aibă 6 cifre!");
+    return;
+  }
+  else if (CNPcopil.length !== 13) {
+    alert("CNP-ul copilului trebuie să aibă 13 cifre!");
+    return;
+  }
+
+  // Crează conținut de fișier text
+  var fileContent = "Punctaj: " + punctajobtinut + "\n" +
+                    "Risc: " + riscobtinut + "\n" +
+                    "Recomandări: " + recomandariobtinute + "\n" +
+                    "CUI Furnizor: " + CUIfurnizor + "\n" +
+                    "Cod parafă medic: " + codparafamedic + "\n" +
+                    "CNP copil: " + CNPcopil + "\n" +
+                    "Vârstă copil: " + varstaobtinuta + "\n" +
+                    "Data completării chestionarului: " + dataformatata + "\n";
+
+  // Crează un nou obiect Blob cu conținutul fișierului
+  var file = new Blob([fileContent], {type: 'text/plain'});
+
+  // Crează un nou obiect URL cu obiectul Blob
+  var fileURL = URL.createObjectURL(file);
+
+  // Crează un nou element de ancorare cu adresa URL a fișierului
+  var downloadLink = document.createElement("a");
+  downloadLink.href = fileURL;
+  downloadLink.download = "export_chestionar-tsa_CNP-" + CNPcopil + ".txt";
+
+  // Adaugă elementul de ancorare la corpul documentului
+  document.body.appendChild(downloadLink);
+
+  // Apasă pe elementul de ancorare pentru a declanșa descărcarea
+  downloadLink.click();
+
+  // Scoate elementul de ancorare din corpul documentului
+  document.body.removeChild(downloadLink);
+}
+
+function anuleazaexportul() {
+  let confirmare = confirm("Sigur vreți să anulați procesul de export și să pierdeți permanent datele introduse? OK = DA; Cancel = NU");
+  if (confirmare) {
+    let url = "https://miculpionier.ro/govro/siui/chestionar-tsa";
+    window.location.href = url;
+  }
+}
